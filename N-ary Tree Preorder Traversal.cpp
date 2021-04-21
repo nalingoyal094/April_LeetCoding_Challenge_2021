b4329974f916789a79a27fbc622097557eca4616
nalingoyal094 <52338664+nalingoyal094@users.noreply.github.com>
2021-04-21 09:20:11 +0530

    Create N-ary Tree Preorder Traversal.cpp

---
+++ N-ary Tree Preorder Traversal.cpp
@@ -0,0 +1,74 @@
+//RECURSIVE SOLUTION
+
+class Solution {
+public:
+    vector<int >ans;
+    
+    
+    void pre(Node*root)
+    {
+        if (root==NULL)
+            return;
+        
+        ans.push_back(root->val);
+        
+        for (auto v: root->children)
+        
+        pre(v);
+        
+        
+    }
+    
+    
+    vector<int> preorder(Node* root) {
+        
+       pre(root);
+        return ans;
+        
+    }
+};
+
+//T.C = O(N)
+//S.C = O(height of the tree) because of the maximum number of function calls in a recursive stack
+
+
+
+
+//ITERATIVE
+
+
+class Solution {
+public:
+    vector<int >ans;
+    
+    void pre(Node*root)
+    {
+        stack<Node*>s;
+        s.push(root);
+        
+        while(s.empty()==false)
+        {
+            Node*t=s.top();
+            s.pop();
+            ans.push_back(t->val);
+            for (int i=(t->children).size()-1;i>=0;i--)
+            {
+                s.push((t->children)[i]);
+            }
+        }
+    }
+    
+    vector<int> preorder(Node* root) {
+        if (root!=NULL)
+        pre(root);
+        return ans;
+        
+        
+    }
+};
+
+
+
+
+
+
